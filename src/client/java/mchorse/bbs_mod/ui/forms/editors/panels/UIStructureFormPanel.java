8a8ede56543e821e164fe2bc1b5c18fb4ee9f3e6
ElGatoPro300 <catgamer207@gmail.com>
2025-11-11 01:37:43 -0600

    adaptation of the nbt method in 1.20.1

--- src/client/java/mchorse/bbs_mod/ui/forms/editors/panels/UIStructureFormPanel.java
+++ src/client/java/mchorse/bbs_mod/ui/forms/editors/panels/UIStructureFormPanel.java
@@ -23,7 +23,6 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtIo;
-import net.minecraft.nbt.NbtTagSizeTracker;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
@@ -167,7 +166,7 @@ public class UIStructureFormPanel extends UIFormPanel<StructureForm>
 
         try (java.io.InputStream is = BBSMod.getProvider().getAsset(Link.assets(path)))
         {
-            NbtCompound root = NbtIo.readCompressed(is, NbtTagSizeTracker.ofUnlimitedBytes());
+            NbtCompound root = NbtIo.readCompressed(is);
 
             int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE, minZ = Integer.MAX_VALUE;
             int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE, maxZ = Integer.MIN_VALUE;
--- src/client/java/mchorse/bbs_mod/ui/framework/elements/input/keyframes/factories/UIPivotSettingsKeyframeFactory.java
+++ src/client/java/mchorse/bbs_mod/ui/framework/elements/input/keyframes/factories/UIPivotSettingsKeyframeFactory.java
@@ -19,7 +19,6 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtIo;
-import net.minecraft.nbt.NbtTagSizeTracker;
 import net.minecraft.client.MinecraftClient;
 
 /**
@@ -88,7 +87,7 @@ public class UIPivotSettingsKeyframeFactory extends UIKeyframeFactory<PivotSetti
 
         try (java.io.InputStream is = BBSMod.getProvider().getAsset(Link.assets(path)))
         {
-            NbtCompound root = NbtIo.readCompressed(is, NbtTagSizeTracker.ofUnlimitedBytes());
+            NbtCompound root = NbtIo.readCompressed(is);
 
             int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE, minZ = Integer.MAX_VALUE;
             int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE, maxZ = Integer.MIN_VALUE;
