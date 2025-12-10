5c6e1a4b26be1eb6ff49df62b5c1b87e460e0d89
ElGatoPro300 <catgamer207@gmail.com>
2025-12-09 21:30:01 -0600

    control volume of audio clips

--- src/main/java/mchorse/bbs_mod/camera/clips/misc/AudioClip.java
+++ src/main/java/mchorse/bbs_mod/camera/clips/misc/AudioClip.java
@@ -15,6 +15,8 @@ public class AudioClip extends CameraClip
 
     public ValueLink audio = new ValueLink("audio", null);
     public ValueInt offset = new ValueInt("offset", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    /* Volumen máximo del clip: 0–100 (por defecto 100) */
+    public ValueInt volume = new ValueInt("volume", 100, 0, 100);
 
     public AudioClip()
     {
@@ -22,6 +24,7 @@ public class AudioClip extends CameraClip
 
         this.add(this.audio);
         this.add(this.offset);
+        this.add(this.volume);
     }
 
     @Override
