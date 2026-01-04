56e60f329699bdaa35d2d350c2a3ef839474259a
ElGatoPro300 <catgamer207@gmail.com>
2026-01-04 10:52:15 -0600

    temporary solution for form selection

--- src/client/java/mchorse/bbs_mod/ui/framework/elements/utils/Batcher2D.java
+++ src/client/java/mchorse/bbs_mod/ui/framework/elements/utils/Batcher2D.java
@@ -29,6 +29,8 @@ public class Batcher2D
 {
     private static FontRenderer fontRenderer = new FontRenderer();
 
+    // private List<Area> scissorStack = new ArrayList<>();
+
     private DrawContext context;
     private FontRenderer font;
 
