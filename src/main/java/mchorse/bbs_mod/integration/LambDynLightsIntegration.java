59a2192b6cd06448e8f10f5cdb64a2eb1402de5f
ElGatoPro300 <catgamer207@gmail.com>
2026-01-28 23:00:54 -0600

    fix the lights form

--- src/main/java/mchorse/bbs_mod/integration/LambDynLightsIntegration.java
+++ src/main/java/mchorse/bbs_mod/integration/LambDynLightsIntegration.java
@@ -1,8 +1,9 @@
 package mchorse.bbs_mod.integration;
 
-import dev.lambdaurora.lambdynlights.api.DynamicLightHandlers;
+import com.mojang.serialization.MapCodec;
 import dev.lambdaurora.lambdynlights.api.DynamicLightsContext;
 import dev.lambdaurora.lambdynlights.api.DynamicLightsInitializer;
+import dev.lambdaurora.lambdynlights.api.entity.luminance.EntityLuminance;
 import dev.lambdaurora.lambdynlights.api.item.ItemLightSourceManager;
 import mchorse.bbs_mod.BBSMod;
 import mchorse.bbs_mod.entity.ActorEntity;
@@ -10,30 +11,61 @@ import mchorse.bbs_mod.entity.GunProjectileEntity;
 import mchorse.bbs_mod.forms.forms.Form;
 import mchorse.bbs_mod.forms.forms.LightForm;
 import mchorse.bbs_mod.morphing.Morph;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
 
 public class LambDynLightsIntegration implements DynamicLightsInitializer
 {
+    private static final BBSModEntityLuminance LUMINANCE_PROVIDER = new BBSModEntityLuminance();
+    private static final EntityLuminance.Type TYPE = EntityLuminance.Type.register(
+        Identifier.of("bbs_mod", "form_light"),
+        MapCodec.unit(LUMINANCE_PROVIDER)
+    );
+
     @Override
     public void onInitializeDynamicLights(DynamicLightsContext context)
-    {}
-
-    public void onInitializeDynamicLights(ItemLightSourceManager itemLightSourceManager)
     {
-        DynamicLightHandlers.registerDynamicLightHandler(BBSMod.ACTOR_ENTITY, (ActorEntity entity) -> getLightLevelFromForm(entity.getForm()));
-        DynamicLightHandlers.registerDynamicLightHandler(BBSMod.GUN_PROJECTILE_ENTITY, (GunProjectileEntity entity) -> getLightLevelFromForm(entity.getForm()));
-        DynamicLightHandlers.registerDynamicLightHandler(EntityType.PLAYER, (PlayerEntity player) ->
+        context.entityLightSourceManager().onRegisterEvent().register(Identifier.of("bbs_mod", "dynamic_lights"), registerContext ->
         {
-            Morph morph = Morph.getMorph(player);
+            registerContext.register(BBSMod.ACTOR_ENTITY, LUMINANCE_PROVIDER);
+            registerContext.register(BBSMod.GUN_PROJECTILE_ENTITY, LUMINANCE_PROVIDER);
+            registerContext.register(EntityType.PLAYER, LUMINANCE_PROVIDER);
+        });
+    }
 
-            if (morph == null)
+    private static class BBSModEntityLuminance implements EntityLuminance
+    {
+        @Override
+        public EntityLuminance.Type type()
+        {
+            return TYPE;
+        }
+
+        @Override
+        public int getLuminance(ItemLightSourceManager itemManager, Entity entity)
+        {
+            if (entity instanceof ActorEntity actor)
             {
-                return 0;
+                return getLightLevelFromForm(actor.getForm());
+            }
+            else if (entity instanceof GunProjectileEntity projectile)
+            {
+                return getLightLevelFromForm(projectile.getForm());
+            }
+            else if (entity instanceof PlayerEntity player)
+            {
+                Morph morph = Morph.getMorph(player);
+
+                if (morph != null)
+                {
+                    return getLightLevelFromForm(morph.getForm());
+                }
             }
 
-            return getLightLevelFromForm(morph.getForm());
-        });
+            return 0;
+        }
     }
 
     private static int getLightLevelFromForm(Form form)
