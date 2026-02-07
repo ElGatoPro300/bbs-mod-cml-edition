1e7e588927f2d2b89eba0be46b4838f203fef905
Fanyel <fanyel.jpeg@gmail.com>
2026-02-07 22:20:26 +0000

    Fixed trigger issues with Sinytra Connector

--- src/main/java/mchorse/bbs_mod/BBSMod.java
+++ src/main/java/mchorse/bbs_mod/BBSMod.java
@@ -203,7 +203,7 @@ public class BBSMod implements ModInitializer
         .dropsNothing()
         .nonOpaque()
         .notSolid()
-        .strength(0F));
+        .strength(-1F, 3600000F));
 
     public static final Block CHROMA_RED_BLOCK = createChromaBlock();
     public static final Block CHROMA_GREEN_BLOCK = createChromaBlock();
