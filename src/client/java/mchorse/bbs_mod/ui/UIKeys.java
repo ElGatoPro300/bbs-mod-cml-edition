3b5094b3a7a5463f012308e093a31c5553a785f3
ElGatoPro300 <catgamer207@gmail.com>
2026-03-11 22:35:51 -0600

    Add read log to the news panel

--- src/client/java/mchorse/bbs_mod/ui/UIKeys.java
+++ src/client/java/mchorse/bbs_mod/ui/UIKeys.java
@@ -831,6 +831,7 @@ public class UIKeys
     public static final IKey NEWS_SEARCH = L10n.lang("bbs.ui.news.search");
     public static final IKey NEWS_RELOAD = L10n.lang("bbs.ui.news.reload");
     public static final IKey NEWS_EMPTY = L10n.lang("bbs.ui.news.empty");
+    public static final IKey NEWS_UPDATED = L10n.lang("bbs.ui.news.updated");
     public static final IKey OVERLAYS_SOUNDS_ADD_MODE = L10n.lang("bbs.ui.overlays.sounds.add_mode");
     public static final IKey OVERLAYS_SOUNDS_FOLDER_MODE = L10n.lang("bbs.ui.overlays.sounds.folder_mode");
     public static final IKey OVERLAYS_SOUNDS_LIKE_MODE = L10n.lang("bbs.ui.overlays.sounds.like_mode");
